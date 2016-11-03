5ee76cfe0bbf77ce8716463ded62acdfa159c8fb
zhouhaijun <542056927@qq.com>
2016-11-03 18:05:02 +0800

    Loading

--- src/router/route-config.js
+++ src/router/route-config.js
@@ -19,9 +19,9 @@ function requireAuth (to, from, next) {
 }
 export default new VueRouter({
   routes: [
-    { path: '/home', component: require('../components/Home.vue'), redirect: '/home/a/b',
+    { path: '/home', component: require('../components/Home.vue'), redirect: '/home/d',
 		children:[
-				{path: '/home/a', component: require('../components/HomeA.vue'), redirect: '/home/a/a',
+				{path: '/home/a', component: require('../components/HomeA.vue'), redirect: '/home/a/b',
 						children:[
 							{path:'/home/a/a',component:require('../components/HomeAa.vue')},
 							{path:'/home/a/b',component:require('../components/HomeAb.vue')}
